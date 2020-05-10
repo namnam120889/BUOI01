bcf4ce27f46f846f4e7de51d9d72cb094966d506
NAMNAM <namnam120889@gmail.com>
2020-05-10 10:27:27 +0700

    UPLOAD LAN 1

---
+++ BUOI01/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BUOI01.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //TAO ACTION KHAC
+        public string CHUOi()
+        {
+            return "Hello";
+        }
+
+        public int SO()
+        {
+            return new Random().Next(1000,10000);
+        }
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
